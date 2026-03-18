ab918134104cc273cd200a57e319dd91174fc155
Zhiyou-sys <zhuhanqi807@gmail.com>
2026-03-18 08:54:50 +0800

    Delete AURA0.1 directory

--- AURA0.1/src/app/App.tsx
+++
@@ -1,7 +0,0 @@
-import { RouterProvider } from "react-router";
-import { router } from "./routes";
-
-export default function App() {
-  // The app routing and rendering happens here
-  return <RouterProvider router={router} />;
-}
